32ac328b56e0c60ce3b9744bf318f5582be5eabf
Brady Maki <78286012+bradym05@users.noreply.github.com>
2025-04-22 00:04:12 -0600

    Client Side Validation
    Added a quick validation step in the submit process to make sure the file is .docx (this is also validated on the server)
    
    Removed support for PDF until .docx is complete

--- scripts/upload.js
+++ scripts/upload.js
@@ -1,5 +1,5 @@
-// Request URL for my server
-const fileUploadURL = "https://server-resume-optimizer.onrender.com/uploadfile/"
+const fileUploadURL = "https://server-resume-optimizer.onrender.com/uploadfile/"; // Request URL for my server
+const fileMIME = "application/vnd.openxmlformats-officedocument.wordprocessingml.document" // MIME for docx files
 
 // Site elements
 var fileButtonElement = document.getElementById("resume-file");
@@ -8,10 +8,10 @@ var submitButtonElement = document.getElementById("file-submit");
 /*********** Upload Resume to Server **********/
 
 function onSubmit() {
-    // Check for a file
     let resume = fileButtonElement.files[0];
-    if (!resume){
-        alert("Please upload a valid resume file (docx or pdf)");
+    // Validate file exists and file type is docx
+    if (!resume || (!resume.type.match(fileMIME))){
+        alert("Please upload a valid resume file (.docx)");
         return;
     }
     // Create data
