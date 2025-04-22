c03c368c0b819e13ab7729caedada4ecfd2f5265
Brady Maki <78286012+bradym05@users.noreply.github.com>
2025-04-22 01:47:21 -0600

    Job Description
    Input job description alongside resume in same request

--- scripts/upload.js
+++ scripts/upload.js
@@ -1,22 +1,33 @@
 const fileUploadURL = "https://server-resume-optimizer.onrender.com/uploadfile/"; // Request URL for my server
-const fileMIME = "application/vnd.openxmlformats-officedocument.wordprocessingml.document" // MIME for docx files
+const fileMIME = "application/vnd.openxmlformats-officedocument.wordprocessingml.document"; // MIME for docx files
+const minJobDescriptionLength = 100; // Minimum character count for job description input
 
 // Site elements
 var fileButtonElement = document.getElementById("resume-file");
 var submitButtonElement = document.getElementById("file-submit");
+var jobDescriptionElement = document.getElementById("job-description");
 
 /*********** Upload Resume to Server **********/
 
 function onSubmit() {
-    let resume = fileButtonElement.files[0];
     // Validate file exists and file type is docx
+    let resume = fileButtonElement.files[0];
     if (!resume || (!resume.type.match(fileMIME))){
         alert("Please upload a valid resume file (.docx)");
+        return;
+    }
+    // Validate job description exists
+    let jobDescription = jobDescriptionElement.value;
+    console.log(jobDescription.length)
+    if (jobDescription.length < minJobDescriptionLength){
+        alert("Please paste at least 100 characters from the job posting you are applying to");
         return;
     }
     // Create data
     let formData = new FormData();
     formData.append('file', resume);
+    formData.append('job_description', jobDescription)
+    // Send post request
     fetch(fileUploadURL, {
         method: 'POST',
         mode: 'cors',
