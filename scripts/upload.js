d3c8db3d4ea38cd8c4ff9a2c9f150442bac1afbc
Brady Maki <78286012+bradym05@users.noreply.github.com>
2025-04-21 23:03:56 -0600

    Basic File Uploading
    Upload files to my server with JS

---
+++ scripts/upload.js
@@ -0,0 +1,31 @@
+// Request URL for my server
+const fileUploadURL = "https://server-resume-optimizer.onrender.com/uploadfile/"
+
+// Site elements
+var fileButtonElement = document.getElementById("resume-file");
+var submitButtonElement = document.getElementById("file-submit");
+
+/*********** Upload Resume to Server **********/
+
+function onSubmit() {
+    // Check for a file
+    let resume = fileButtonElement.files[0];
+    if (!resume){
+        alert("Please upload a valid resume file (docx or pdf)");
+        return;
+    }
+    // Create data
+    let formData = new FormData();
+    formData.append('file', resume);
+    fetch(fileUploadURL, {
+        method: 'POST',
+        body: formData
+    }).then(data => {
+        console.log('Success:', data)
+    }).catch(error => {
+        console.log('Error:', error)
+    });
+}
+
+// Listen for click
+submitButtonElement.addEventListener("click", onSubmit)
