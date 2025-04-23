161783173ee8fe82da0d9f9a8635c0a23eb2717a
Brady Maki <78286012+bradym05@users.noreply.github.com>
2025-04-23 11:57:10 -0600

    General Improvements
    Updated design, client-sided file validation, hints, and character count

--- scripts/upload.js
+++ scripts/upload.js
@@ -1,43 +1,107 @@
 const fileUploadURL = "https://server-resume-optimizer.onrender.com/uploadfile/"; // Request URL for my server
 const fileMIME = "application/vnd.openxmlformats-officedocument.wordprocessingml.document"; // MIME for docx files
+const maxFileSize = 2 * 10e5; // Maximum size (in bytes) for resume file
 const minJobDescriptionLength = 100; // Minimum character count for job description input
+const maxJobDescriptionLength = 2000; // Maximum character count for job description input
 
 // Site elements
+var fileHintElement = document.getElementById("file-hint")
 var fileButtonElement = document.getElementById("resume-file");
 var submitButtonElement = document.getElementById("file-submit");
 var jobDescriptionElement = document.getElementById("job-description");
+var characterCountElement = document.getElementById("character-count");
+
+// State
+var processingRequest = false;
+
+/*************** Job Description **************/
+
+function updateCharacterCount() {
+    // Update length
+    characterCountElement.innerText = `${jobDescriptionElement.value.length}/${maxJobDescriptionLength}`;
+}
+
+// Input field attributes
+jobDescriptionElement.setAttribute("minlength", minJobDescriptionLength);
+jobDescriptionElement.setAttribute("maxlength", maxJobDescriptionLength);
+jobDescriptionElement.setAttribute("placeholder", `Paste job description here (Maximum ${maxJobDescriptionLength} Characters)`);
+
+// Update character count on key up
+jobDescriptionElement.addEventListener("keyup", updateCharacterCount);
+updateCharacterCount();
+
+/**************** Upload Hint *****************/
+
+function validateFile() {
+    // Check for file
+    let file = fileButtonElement.files[0];
+    if (file){
+        // Check file size
+        if (file.size > maxFileSize) {
+            alert("File cannot exceed maximum size of 2mb");
+            return false;
+        }
+        // Check file type
+        if (!file.type.match(fileMIME)) {
+            alert("File type must be .docx");
+            return false;
+        }
+        // Update upload hint
+        fileHintElement.innerText = `${file.name}`;
+        return true;
+    }
+    return false;
+}
+
+fileButtonElement.addEventListener("change", validateFile)
+
+/************ Get Optimized Resume ************/
+
+function onUploadSuccess(data) {
+    // Get resume UUID
+    responseFormData = data.formData();
+    resumeId = responseFormData.get("file_id");
+    // 
+}
 
 /*********** Upload Resume to Server **********/
 
 function onSubmit() {
-    // Validate file exists and file type is docx
-    let resume = fileButtonElement.files[0];
-    if (!resume || (!resume.type.match(fileMIME))){
-        alert("Please upload a valid resume file (.docx)");
-        return;
+    // Check if request is processing already
+    if (!processingRequest){
+        // Validate resume file
+        if (!validateFile()) {
+            return false;
+        }
+        // Validate job description exists
+        let jobDescription = jobDescriptionElement.value;
+        if (jobDescription.length < minJobDescriptionLength){
+            alert("Please paste at least 100 characters from the job posting you are applying to");
+            return;
+        }
+        processingRequest = true
+        // Create data
+        let formData = new FormData();
+        formData.append('file', fileButtonElement.files[0]);
+        formData.append('job_description', jobDescription);
+        // Send post request
+        fetch(fileUploadURL, {
+            method: 'POST',
+            mode: 'cors',
+            body: formData
+        }).then(data => {
+            console.log('Success:', data)
+            onUploadSuccess(data)
+        }).catch(error => {
+            console.log('Error:', error)
+        });
+    } else {
+        alert("Already optimizing, please wait")
     }
-    // Validate job description exists
-    let jobDescription = jobDescriptionElement.value;
-    console.log(jobDescription.length)
-    if (jobDescription.length < minJobDescriptionLength){
-        alert("Please paste at least 100 characters from the job posting you are applying to");
-        return;
-    }
-    // Create data
-    let formData = new FormData();
-    formData.append('file', resume);
-    formData.append('job_description', jobDescription)
-    // Send post request
-    fetch(fileUploadURL, {
-        method: 'POST',
-        mode: 'cors',
-        body: formData
-    }).then(data => {
-        console.log('Success:', data)
-    }).catch(error => {
-        console.log('Error:', error)
-    });
 }
+
+// Set file filter for upload button
+fileButtonElement.setAttribute("accept", fileMIME)
 
 // Listen for click
 submitButtonElement.addEventListener("click", onSubmit)
